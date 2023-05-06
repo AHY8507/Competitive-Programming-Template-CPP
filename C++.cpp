27331d6b96a1771f9c16bd86a7549eccca680b60
AmirHossein Yousefi <101252560+AHY8507@users.noreply.github.com>
2023-05-06 20:43:04 +0330

    Add files via upload

---
+++ C++.cpp
@@ -0,0 +1,18 @@
+//In The Name Of God
+//Welcome To My Hell
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define fast_io ios::sync_with_stdio(0);cin.tie(0)
+#define all(a) a.begin(),a.end()
+#define int long long
+
+typedef pair<int , int> pii;
+typedef map<int , int> mii;
+
+int32_t main()
+{
+    fast_io;
+}
